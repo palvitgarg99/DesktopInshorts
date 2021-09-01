d79e571bec74073c09a704624cbb679200f0473c
palvitgarg99 <palvitgarg99@gmail.com>
2021-09-01 05:00:11 -0400

    Setup.py file to setup easily

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='main.py',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/palvitgarg99/DesktopInshorts',
+    license='MIT',
+    author='Palvit Garg',
+    author_email='palvitgarg99@gmail.com',
+    description='DesktopInshorts'
+)
